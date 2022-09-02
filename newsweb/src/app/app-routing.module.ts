4f5c02e0f748efa7350760a4d922c55b3d8bc27c
ori <ori_larusa@hotmail.com>
2022-09-02 14:47:46 -0300

    guardar cosas

--- newsweb/src/app/app-routing.module.ts
+++ newsweb/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
 
 const routes: Routes = [
-  {path:'', component:CardComponent }, //home
+  {path:' ', component:CardComponent }, //home
 ];
 
 @NgModule({
